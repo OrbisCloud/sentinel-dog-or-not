0c72d08c7ecb51cc5447544638df80fb8891fa6e
Ramon Smits <ramonsmits@Ramons-MacBook-Pro.local>
2023-09-28 18:45:16 +0100

    Ensure flask app uses correct port

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ from api.run_api import run_api
 if __name__ == '__main__':
     app: Flask = Flask(__name__)
     app.register_blueprint(run_api, url_prefix='/v1')
-    app.run()
+    app.run(host='0.0.0.0', port=7777)
 
 
